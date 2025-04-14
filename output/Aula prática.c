ee74a6c19d1b233bce4562a71ebc29ac39959a5c
cypreste <raphael.ncypreste@gmail.com>
2025-04-14 18:53:29 -0300

    Aula - 14/04

---
+++ output/Aula prática.c
@@ -0,0 +1,50 @@
+//Super Trunfo em C: desenvolvendo a lógica do jogo
+#include <stdio.h>
+/*
+int main () {
+    float maca = 76.0;
+    float laranja = 75.0;
+
+    if (maca > laranja)
+        printf("A maçã é maior que a laranja\n");
+    else if (maca < laranja)
+        printf("A maçã é menor que a laranja\n");
+    else
+        printf("A maçã é igual a laranja\n");
+    return 0;
+    }  
+    // int main () {
+
+    //     int idade = 15;
+
+    //     if (idade >= 16) 
+    //         printf("Voce pode votar!\n");
+    //      else {
+    //         printf("Voce nao pode votar ainda.\n");
+    //     }
+    //     return 0;
+    // }
+int main () {
+ int nota = 70;
+    printf("A nota do aluno e: %d\n", nota);
+    if (nota >= 60) {
+        printf("Voce foi aprovado! Parabens\n");
+    } else if (nota >= 50) {
+        printf("Voce esta de recuperacao\n");
+    }
+    return 0;
+}
+*/
+    int main () {
+    int preco1 = 80.0;
+    int preco2 = 80.0;
+
+    if (preco1 > preco2)
+        printf("O preco 1 e MAIOR que o preco 2\n");
+    else if (preco1 < preco2)
+        printf("O preco 1 e MENOR que preco 2\n");
+    else
+        printf("O preco 1 e IGUAL ao preco 2\n");
+    return 0;
+
+}
