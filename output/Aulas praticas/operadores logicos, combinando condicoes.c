db847b91bbff8030eb94ed85d8b461aef0021c2f
cypreste <raphael.ncypreste@gmail.com>
2025-04-16 13:02:59 -0300

    praticando em tempo real

--- output/Aulas praticas/operadores logicos, combinando condicoes.c
+++ output/Aulas praticas/operadores logicos, combinando condicoes.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 
 int main () {
-
-    int a = 10, b = 5;
-
-    // Operador lógico AND (&&)
+    int a = -10;
+   /*
+    // Operador lógico AND (&&) - So sera executado se ambas as condições forem verdadeiras
     if (a > 0 && b > 0) {
-        printf("Ambos os numeros sao positivos.\n");
+        printf("\n Ambos os numeros sao positivos.\n");
     } else {
-        printf("Pelo menos um dos numeros e negativo.\n");
+        printf("\n Pelo menos um dos numeros e negativo.\n");
     }
     return 0;	
+}
+    // Operador lógico OR (||) - So sera executado se pelo menos uma das condições for verdadeira
+    if (a > 0 || b > 0) {
+        printf("\n Pelo menos um dos numeros e positivo.\n");
+    } else {
+        printf("\n Ambos os numeros sao negativos.\n");
+    }
+    return 0;	
+}
+    // Operador lógico NOT (!) - Inverte as condicoes
+    if (!(a > 0)) {
+        printf("\n A variavel e negativa.\n");
+    } else {
+        printf("\n A varivel e positivo.\n");
+    }
+    return 0;	
+*/
+    
+
+return 0;
 }
